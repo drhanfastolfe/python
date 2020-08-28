b4a6ccd08b9cdc85644a823205d473aa5eb0cf96
Hamza <hamzzza56@gmail.com>
2020-08-28 11:09:07 +0200

    Finishes casting

---
+++ beginners_tutorial/casting.py
@@ -0,0 +1,10 @@
+x = 1
+print(x)
+
+print(float(x))
+
+str = "5"
+print(str)
+
+print(int(str))
+print(float(str))
--- beginners_tutorial/data_types.py
+++ beginners_tutorial/data_types.py
@@ -1,11 +1,11 @@
 # Integers
-1 2 3 4 5
- 
+# 1 2 3 4 5
+
 # Floats
-1.5 3.14 -0.25
+# 1.5 3.14 -0.25
 
 # Strings
-"4" 'strings'
+# "4" 'strings'
 
 # Booleans
-true false 
+# true false 
